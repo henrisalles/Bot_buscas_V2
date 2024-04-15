011a80baf865be1a283014621f251fa4eda92395
henrisalles <henrique.aguiar.salles@gmail.com>
2024-04-15 01:37:36 -0300

    Concluida a busca de paginas de modo assincrono e a captação dos urls dos produtos para mais uma pesquisa assincrona por todos eles

--- Buscas.py
+++ Buscas.py
@@ -1,14 +1,17 @@
 import asyncio
 import aiohttp
 import time
-
+from bs4 import BeautifulSoup
 
 class Busca():
-    def __init__(self, frase_pesquisa:str, total_paginas:int=15) -> None:
+    def __init__(self, frase_pesquisa:str, total_paginas:int=1) -> None:
+        self.paginas_listas_produto = []
         self.total_paginas = total_paginas
         self.lista_urls = self.cria_lista_urls(frase_pesquisa)
         asyncio.run(self.resquest_async())
-        print("Feito")
+        
+        
+        self.procura_url_produto_em_pagina_lista(self.paginas_listas_produto[0])
 
     def cria_lista_urls(self, frase_pesquisa:str):
         lista_frase = frase_pesquisa.split()
@@ -20,29 +23,38 @@ class Busca():
         for i in range(self.total_paginas):
             pagina_desde_x = i*40 
             lista_urls.append(f"https://lista.mercadolivre.com.br/{frase_p_url}_Desde_{pagina_desde_x}_NoIndex_True")
-        print(lista_urls)
         return lista_urls
     
-
     async def resquest_async(self):
         async with aiohttp.ClientSession() as session:
             ret = await asyncio.gather(*(self.lista_produtos(url, session) for url in self.lista_urls))
         print("Finalized all. Return is a list of len {} outputs.".format(len(ret)))
 
-    
     async def lista_produtos(self, url:str, session):
         try:
             async with session.get(url=url) as response:
-                resp = await response.read()
+                resp = await response.text()
+                print(type(resp))
+                self.paginas_listas_produto.append(resp)
                 print(f"Url {url} pega com sucesso !")
+                return response
         except Exception as e:
             print(f"Não foi possivel pegar a url {url} por uma Exception de {e.__class__}")
 
+    def procura_url_produto_em_pagina_lista(self, response_pagina):
+        soup = BeautifulSoup(response_pagina, "html.parser")
+        url_produtos = []
+        #print(soup)
+        i = 0
+        search_frames = soup.find_all('li', class_="ui-search-layout__item")
+        # Limitando o search_frame para o padrao de busca por pagina
+        search_frames = search_frames[:40]
 
-    
+        for prod in search_frames:
+            url_produtos.append(prod.find('a', class_='ui-search-link')['href'])
+        print(url_produtos[0])
 
 
 
 
-
-#Busca("Tenis Fila Kr6")
+Busca("Tenis Fila Kr6")
